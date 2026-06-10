0a4dc24dd1ffa26b863c6cbc6cf2fd25db8e6d4f
kino-ona <kino@ona.co.kr>
2026-06-10 16:42:28 +0900

    adm > page 업데이트

--- adm/page/post_file_insert.php
+++ adm/page/post_file_insert.php
@@ -1,18 +1,16 @@
 <?php
 include_once('./_common.php');
 
-header('Content-Type: application/json; charset=UTF-8');
-
 //check_admin_token();
 
 $msg = '';
 $act = '';
 
-$img_file = isset($_FILES['file0']) ? $_FILES['file0'] : null;
+$img_file    = $_FILES['file0'];
 $img_url = '';
 $org_name = '';
 
-if ($img_file && is_uploaded_file($img_file['tmp_name'])) {
+ if (is_uploaded_file($img_file['tmp_name'])) {
         
         $year = date('Y');
         
@@ -45,11 +43,10 @@ if ($img_file && is_uploaded_file($img_file['tmp_name'])) {
         // 첨부파일 첨부시 첨부파일명에 공백이 포함되어 있으면 일부 PC에서 보이지 않거나 다운로드 되지 않는 현상이 있습니다.
         $fname = P1_TIME_YMDHIS_ORG.'_'.substr($shuffle,0,8).'.'.$r_file_ext;
         // $fname = abs(ip2long($_SERVER['REMOTE_ADDR'])).'_'.substr($shuffle,0,8).'_'.replace_filename($fname);
-
+        
         // 업로드가 안된다면 에러메세지 출력하고 die
-        if (!move_uploaded_file($fname_tmp, $data_dir.'/'.$fname)) {
-            echo json_encode(array('filename' => '', 'orgname' => '', 'error' => 'upload_failed'), JSON_UNESCAPED_UNICODE);
-            exit;
+        if(!move_uploaded_file($fname_tmp, $data_dir.'/'.$fname)) {
+            alert("file upload error");
         }
         
         // 이미지
@@ -57,4 +54,6 @@ if ($img_file && is_uploaded_file($img_file['tmp_name'])) {
     }
  
 $postData = array('filename' => $img_url, 'orgname' => $org_name);
-echo json_encode($postData, JSON_UNESCAPED_UNICODE);
+echo json_encode($postData);
+
+?>
