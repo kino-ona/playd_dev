842f717ee769e599835e9bfc43a97193c05ce815
kino-ona <kino@ona.co.kr>
2026-06-10 17:42:22 +0900

    관리자 > 리포트, 등록 보완 수정

--- adm/page/post_skin/report/write.php
+++ adm/page/post_skin/report/write.php
@@ -313,8 +313,12 @@ function send() {
         
     if(confirm("<?=$html_title?> 하시겠습니까?")){
         
-        <?php echo $editor_js;?> // 에디터 사용시 자바스크립트에서 내용을 폼필드로 넣어주며 내용이 입력되었는지 검사함   
-         oEditors.getById["b_cont"].exec("UPDATE_CONTENTS_FIELD",[]);      
+    // <?php echo $editor_js;?> // 에디터 사용시 자바스크립트에서 내용을 폼필드로 넣어주며 내용이 입력되었는지 검사함   
+    //      oEditors.getById["b_cont"].exec("UPDATE_CONTENTS_FIELD",[]);    
+        <?php echo $editor_js;?> // 에디터 사용시 자바스크립트에서 내용을 폼필드로 넣어주며 내용이 입력되었는지 검사함
+        if (typeof oEditors !== 'undefined' && oEditors.getById["b_cont"]) {
+            oEditors.getById["b_cont"].exec("UPDATE_CONTENTS_FIELD",[]);
+        }
 
 
         var b_site = $("input[name='b_site']:checked").val();
